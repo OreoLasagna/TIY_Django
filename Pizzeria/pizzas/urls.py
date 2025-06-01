8652abc382d87cb2db8c5ac296fc6476a6a9248a
git <bpdaut@outlook.com>
2025-06-01 10:24:39 -0500

    Added a Home page to Pizza project

---
+++ Pizzeria/pizzas/urls.py
@@ -0,0 +1,11 @@
+"""Defines the URLs for the Pizza project"""
+
+from django.urls import path
+
+from . import views
+
+app_name = 'pizzas'
+urlpatterns = [
+    #Home page
+    path('', views.index, name = 'index'),
+]
